6b8fa97c9da2b5b111c98580fd5d0fa86799d771
Bin Gao <gaobin@Bins-MBP.home>
2016-11-16 08:22:04 -0800

    .

--- app.py
+++ app.py
@@ -472,7 +472,7 @@ def processRequest(req):
         try:
             rowList = AirInfo.query.filter(AirInfo.flightNumber == int(flightNumber)) \
                                     .filter(AirInfo.status.contains("Delayed")).all()
-            delayedInfo = AirInfo.status
+            delayedInfo = str(AirInfo.status)
         except:
             db.session.rollback()
 
